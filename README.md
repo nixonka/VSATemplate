d378198d50f4b1d37ed48825367fdf69a8f2e9ef
Nikola Simic <1775516+nixonka@users.noreply.github.com>
2023-12-06 15:22:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Vertical Slice Architecture example in .NET 8
+
+
+
+## Inspired by
+
+- [Vertical Slice Architecture solution template by Nadir Badnjevic](https://github.com/nadirbad/VerticalSliceArchitecture)
+- [Clean Architecture solution template by Jason Taylor](https://github.com/jasontaylordev/CleanArchitecture)
+- [Vertical slice architecture by Jimmy Bogard](https://jimmybogard.com/vertical-slice-architecture/)
+- [Organize code by Feature using Vertical Slices by Derek Comartin](https://codeopinion.com/organizing-code-by-feature-using-vertical-slices/)
+
+## License
+
+This project is licensed with the [MIT license](./LICENSE).
---
+++ src/Application/Features/TodoItems/UpdateTodoItem.cs
@@ -0,0 +1,66 @@
+﻿using FluentValidation;
+
+using MediatR;
+
+using Microsoft.AspNetCore.Mvc;
+
+using VerticalSliceArchitecture.Application.Common;
+using VerticalSliceArchitecture.Application.Common.Exceptions;
+using VerticalSliceArchitecture.Application.Entities;
+using VerticalSliceArchitecture.Application.Infrastructure.Persistence;
+
+namespace VerticalSliceArchitecture.Application.Features.TodoItems;
+
+public class TodoItemsController : ApiControllerBase
+{
+    [HttpPut("/api/todo-items/{id}")]
+    public async Task<ActionResult> Update(int id, UpdateTodoItemCommand command)
+    {
+        if (id != command.Id)
+        {
+            return BadRequest();
+        }
+
+        await Mediator.Send(command);
+
+        return NoContent();
+    }
+}
+
+public class UpdateTodoItemCommand : IRequest
+{
+    public int Id { get; set; }
+
+    public string? Title { get; set; }
+
+    public bool Done { get; set; }
+}
+
+public class UpdateTodoItemCommandValidator : AbstractValidator<UpdateTodoItemCommand>
+{
+    public UpdateTodoItemCommandValidator()
+    {
+        RuleFor(v => v.Title)
+            .MaximumLength(200)
+            .NotEmpty();
+    }
+}
+
+internal sealed class UpdateTodoItemCommandHandler(ApplicationDbContext context) : IRequestHandler<UpdateTodoItemCommand>
+{
+    public async Task Handle(UpdateTodoItemCommand request, CancellationToken cancellationToken)
+    {
+        var entity = await context.TodoItems
+            .FindAsync(new object[] { request.Id }, cancellationToken);
+
+        if (entity == null)
+        {
+            throw new NotFoundException(nameof(TodoItem), request.Id);
+        }
+
+        entity.Title = request.Title;
+        entity.Done = request.Done;
+
+        await context.SaveChangesAsync(cancellationToken);
+    }
+}
